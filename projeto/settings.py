9ad3a647d83fc564097e6209fcb5732d85ffa183
Bruno Feiertag Martini <feiertagbruno@gmail.com>
2024-03-23 14:12:17 -0400

    inicio models

--- projeto/settings.py
+++ projeto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y-#vztpc8gjwnad(io9mb-_b$fh5#$2g(s5+e2evq@y#&9o#ip
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'temp'
+            BASE_DIR / 'base_templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -119,6 +119,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
-from .views import *
+from django.urls import path #re_path caso queria usar expressões regulares (pesquisar)
+from .views import * #from . import views
+
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
+    path('', home, name="home"),
+    path('recipes/<int:id>/', recipe, name="recipe")
 ]
